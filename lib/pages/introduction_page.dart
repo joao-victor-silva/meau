5eb393d2b093a3aef5bfe14cf91456445ff519ad
João Victor Silva <victor.joao.1611@gmail.com>
2022-12-06 01:25:22 -0300

    feat: splash and introduction pages

--- lib/pages/introduction_page.dart
+++ lib/pages/introduction_page.dart
@@ -1,64 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:meau/pages/animal_signup.dart';
-import 'package:meau/pages/local_user_signin.dart';
-import 'package:meau/pages/local_user_signup.dart';
-import 'package:meau/pages/splash_page.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  final Future<FirebaseApp> _firebaseApp = Firebase.initializeApp();
-  late FirebaseFirestore _database;
-  late FirebaseAuth _auth;
-
-// This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Meau',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: FutureBuilder(
-          future: _firebaseApp,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text('You have an error! ${snapshot.error.toString()}');
-            } else if (snapshot.hasData) {
-              _database = FirebaseFirestore.instance;
-              _auth = FirebaseAuth.instance;
-              return LocalUserSignUpPage(
-                database: _database,
-                auth: _auth,
-              );
-              // return LocalUserSignInPage(database: _database, auth: _auth);
-              // return AnimalSignUpPage(database: _database, auth: _auth);
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class IntroductionPage extends StatelessWidget {
+  const IntroductionPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -107,7 +50,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   backgroundColor: MaterialStatePropertyAll(
                       Color.fromARGB(255, 255, 211, 88)),
                   foregroundColor:
-                      MaterialStatePropertyAll(Color.fromARGB(255, 67, 67, 67)),
+                  MaterialStatePropertyAll(Color.fromARGB(255, 67, 67, 67)),
                   fixedSize: MaterialStatePropertyAll(Size(232, 40)),
                   textStyle: MaterialStatePropertyAll(
                     TextStyle(
@@ -126,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   backgroundColor: MaterialStatePropertyAll(
                       Color.fromARGB(255, 255, 211, 88)),
                   foregroundColor:
-                      MaterialStatePropertyAll(Color.fromARGB(255, 67, 67, 67)),
+                  MaterialStatePropertyAll(Color.fromARGB(255, 67, 67, 67)),
                   fixedSize: MaterialStatePropertyAll(Size(232, 40)),
                   textStyle: MaterialStatePropertyAll(
                     TextStyle(
@@ -145,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   backgroundColor: MaterialStatePropertyAll(
                       Color.fromARGB(255, 255, 211, 88)),
                   foregroundColor:
-                      MaterialStatePropertyAll(Color.fromARGB(255, 67, 67, 67)),
+                  MaterialStatePropertyAll(Color.fromARGB(255, 67, 67, 67)),
                   fixedSize: MaterialStatePropertyAll(Size(232, 40)),
                   textStyle: MaterialStatePropertyAll(
                     TextStyle(
